7239073c216e37b1b46ec9ef79d5f5acfb7274e2
nkwaaaa <nkwaaaa@gmail.com>
2023-08-02 11:15:29 -0300

    add db config

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+import { createPool } from "mysql2/promise"
+
+const pool = createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+})
+
+export { pool }
