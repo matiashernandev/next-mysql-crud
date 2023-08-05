e090a9cece92c8cf157252fdf587133f7ea2c4e1
nkwaaaa <nkwaaaa@gmail.com>
2023-08-05 17:20:02 -0300

    add delete product

--- src/pages/api/products/[id].js
+++ src/pages/api/products/[id].js
@@ -1,11 +1,26 @@
 import { pool } from "@/config/db"
 
 export default async function handler(req, res) {
+  switch (req.method) {
+    case "GET":
+      return getProduct(req, res)
+
+    case "DELETE":
+      return deleteProduct(req, res)
+
+    default:
+      return res.status(405).json({ message: "Method not allowed" })
+  }
+}
+
+const getProduct = async (req, res) => {
   const { id } = req.query
+  const [result] = await pool.query(`SELECT * FROM product WHERE id = ?`, [id])
+  return res.status(200).json(result[0])
+}
 
-  const [result] = await pool.query(`SELECT * FROM product WHERE id = ${id}`)
-
-  console.log(result)
-
-  return res.status(200).json(result)
+const deleteProduct = async (req, res) => {
+  const { id } = req.query
+  const [result] = await pool.query(`DELETE FROM product WHERE id = ?`, [id])
+  return res.status(204).json()
 }
--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,10 +1,16 @@
 import Layout from "@/components/Layout"
+import { Button } from "@material-tailwind/react"
 import axios from "axios"
+import { useRouter } from "next/router"
 
-export default function ProductView({ data }) {
-  console.log(data[0])
+export default function ProductView({ product }) {
+  const { push } = useRouter()
 
-  const product = data[0]
+  const handleDelete = async () => {
+    await axios.delete("/api/products/" + product.id)
+    push("/")
+  }
+
   return (
     <Layout>
       {product ? (
@@ -13,6 +19,10 @@ export default function ProductView({ data }) {
           <p>{product.id}</p>
           <p>{product.description}</p>
           <p>{product.price}</p>
+
+          <Button onClick={handleDelete} color="red">
+            gfdgd
+          </Button>
         </div>
       ) : (
         <div>No product found</div>
@@ -22,15 +32,13 @@ export default function ProductView({ data }) {
 }
 
 export async function getServerSideProps(context) {
-  const { data } = await axios.get(
+  const { data: product } = await axios.get(
     "http://localhost:3000/api/products/" + context.query.id
   )
 
-  console.log(data)
-
   return {
     props: {
-      data,
+      product,
     },
   }
 }
